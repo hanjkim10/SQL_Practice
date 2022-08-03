bc2895f1642433ef7bcad51e49968015ef83ecea
hanjkim10 <hanj.kim.10@gmail.com>
2022-08-03 16:38:05 +0900

    JOIN

--- programmersSQLJOIN_4.sql
+++ programmersSQLJOIN_4.sql
@@ -28,7 +28,7 @@
 
 -- SEX_UPON_OUTCOME	VARCHAR(N)	FALSE
 
--- 천재지변으로 인해 일부 데이터가 유실되었습니다. 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
+-- 보호소에서 중성화 수술을 거친 동물 정보를 알아보려 합니다. 보호소에 들어올 당시에는 중성화1되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회하는 SQL 문을 작성해주세요.
 
 -- 예시
 
@@ -38,40 +38,51 @@
 
 -- ANIMAL_ID	ANIMAL_TYPE	DATETIME	INTAKE_CONDITION	NAME	SEX_UPON_INTAKE
 
--- A352713	Cat	2017-04-13 16:29:00	Normal	Gia	Spayed Female
+-- A367438	Dog	2015-09-10 16:01:00	Normal	Cookie	Spayed Female
 
--- A350375	Cat	2017-03-06 15:01:00	Normal	Meo	Neutered Male
+-- A382192	Dog	2015-03-13 13:14:00	Normal	Maxwell 2	Intact Male
+
+-- A405494	Dog	2014-05-16 14:17:00	Normal	Kaila	Spayed Female
+
+-- A410330	Dog	2016-09-11 14:09:00	Sick	Chewy	Intact Female
 
 -- ANIMAL_OUTS
 
 -- ANIMAL_ID	ANIMAL_TYPE	DATETIME	NAME	SEX_UPON_OUTCOME
 
--- A349733	Dog	2017-09-27 19:09:00	Allie	Spayed Female
+-- A367438	Dog	2015-09-12 13:30:00	Cookie	Spayed Female
 
--- A352713	Cat	2017-04-25 12:25:00	Gia	Spayed Female
+-- A382192	Dog	2015-03-16 13:46:00	Maxwell 2	Neutered Male
 
--- A349990	Cat	2018-02-02 14:18:00	Spice	Spayed Female
+-- A405494	Dog	2014-05-20 11:44:00	Kaila	Spayed Female
 
--- ANIMAL_OUTS 테이블에서
+-- A410330	Dog	2016-09-13 13:46:00	Chewy	Spayed Female
 
--- Allie의 ID는 ANIMAL_INS에 없으므로, Allie의 데이터는 유실되었습니다.
+-- Cookie는 보호소에 들어올 당시에 이미 중성화되어있었습니다.
 
--- Gia의 ID는 ANIMAL_INS에 있으므로, Gia의 데이터는 유실되지 않았습니다.
+-- Maxwell 2는 보호소에 들어온 후 중성화되었습니다.
 
--- Spice의 ID는 ANIMAL_INS에 없으므로, Spice의 데이터는 유실되었습니다.
+-- Kaila는 보호소에 들어올 당시에 이미 중성화되어있었습니다.
+
+-- Chewy는 보호소에 들어온 후 중성화되었습니다.
 
 -- 따라서 SQL문을 실행하면 다음과 같이 나와야 합니다.
 
--- ANIMAL_ID	NAME
+-- ANIMAL_ID	ANIMAL_TYPE	NAME
 
--- A349733	Allie
+-- A382192	Dog	Maxwell 2
 
--- A349990	Spice
+-- A410330	Dog	Chewy
 
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
-WHERE ANIMAL_ID NOT IN (
-        SELECT ANIMAL_ID
-        FROM ANIMAL_INS
+SELECT
+    animal_ins.animal_id,
+    animal_ins.animal_type,
+    animal_ins.name
+FROM animal_ins
+    JOIN animal_outs on animal_ins.animal_id = animal_outs.animal_id
+WHERE
+    animal_ins.sex_upon_outcome like 'Intact%'
+    AND (
+        animal_outs.sex_upon_outcome like 'Spayed%'
+        OR animal_outs.sex_upon_outcome like 'Neutered%'
     )
-ORDER BY ANIMAL_ID
